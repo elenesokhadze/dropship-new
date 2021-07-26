d74186a2aac2455ffd89db6dbeb9ad9b68fa08db
elenesokhadze <elenesoxadze@gmail.com>
2021-07-26 20:17:28 +0400

    feat: add theme

--- src/index.js
+++ src/index.js
@@ -7,20 +7,33 @@ import { Provider } from "react-redux";
 import { applyMiddleware, compose, createStore } from "redux";
 import combinedReducer from "./redux/products/index";
 import thunk from "redux-thunk";
-const composeEnchanser = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { createTheme, MuiThemeProvider } from "@material-ui/core";
 
+const composeEnchanser = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combinedReducer,
   composeEnchanser(applyMiddleware(thunk))
 );
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#61D5DF",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#49547D",
+    },
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </MuiThemeProvider>
   </React.StrictMode>,
 
   document.getElementById("root")
