deb0f81005f958d4e1d62cda686170a0a0752d45
elenesokhadze <elenesoxadze@gmail.com>
2021-08-01 18:58:25 +0400

    fix: some design changes

--- src/ModifyProduct/AddProductModal.js
+++ src/ModifyProduct/AddProductModal.js
@@ -8,6 +8,7 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 import { addProduct } from "../API";
 import { getProducts } from "../redux/products/ProductActions";
+import ReactDom from "react-dom";
 
 const productSchema = yup.object({
   title: yup
@@ -22,9 +23,29 @@ const productSchema = yup.object({
   imageUrl: yup.string().url("Input must be a url"),
 });
 
-const AddProductModal = () => {
+const AddProductModal = ({ children }) => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const MODAL_STYLES = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#FFF",
+    zIndex: 1000,
+    width: 630,
+    height: 651,
+  };
+
+  const OVERLAY_STYLES = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0, 0, 0, .7)",
+    zIndex: 1000,
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -53,93 +74,99 @@ const AddProductModal = () => {
     history.push("/catalog");
   };
 
-  return (
-    <div className="modal__container" onClick={handleClose}>
-      <div className="modal-product__dialog">
-        <div className="modify-product__title">
-          <h2 className="modify-product__title--text">Add Product</h2>
-          <i className="fas fa-plus"></i>
-        </div>
-        <form onSubmit={formik.handleSubmit} className="modify-product__form">
-          <TextField
-            label="Product Title"
-            variant="outlined"
-            name="title"
-            size="medium"
-            value={formik.values.title}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.title && Boolean(formik.errors.title)}
-            helperText={formik.touched.title && formik.errors.title}
-            style={{ width: "500px", color: "grey", margin: "12px 0px" }}
-            color="primary"
-          />
-          <TextField
-            label="Description"
-            variant="outlined"
-            name="description"
-            multiline
-            rows={5}
-            value={formik.values.description}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={
-              formik.touched.description && Boolean(formik.errors.description)
-            }
-            helperText={formik.touched.description && formik.errors.description}
-            style={{ width: "500px", color: "grey", margin: "12px 0px" }}
-            color="primary"
-          />
-          <TextField
-            label="Price"
-            variant="outlined"
-            size="medium"
-            type="number"
-            name="price"
-            value={formik.values.price}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.price && Boolean(formik.errors.price)}
-            helperText={formik.touched.price && formik.errors.price}
-            InputProps={{
-              inputProps: {
-                min: 0,
-              },
-              startAdornment: (
-                <InputAdornment position="start">$</InputAdornment>
-              ),
-            }}
-            style={{ width: "500px", color: "grey", margin: "12px 0px" }}
-            color="primary"
-          />
-          <TextField
-            label="Image Url"
-            variant="outlined"
-            name="imageUrl"
-            value={formik.values.imageUrl}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.imageUrl && Boolean(formik.errors.imageUrl)}
-            helperText={formik.touched.imageUrl && formik.errors.imageUrl}
-            style={{ width: "500px", color: "grey", margin: "12px 0px" }}
-            color="primary"
-          />
-          <div className="modify-product__button">
-            <input
-              type="button"
-              value="Back"
-              className=" modify-product__button--blue"
-              onClick={handleClose}
-            />
-            <input
-              type="submit"
-              value="Save"
-              className=" modify-product__button--blue"
-            />
+  return ReactDom.createPortal(
+    <>
+      <div style={OVERLAY_STYLES} onClick={handleClose}>
+        <div style={MODAL_STYLES}>
+          <div className="modify-product__title">
+            <h2 className="modify-product__title--text">Add Product</h2>
+            <i className="fas fa-plus"></i>
           </div>
-        </form>
+          <form onSubmit={formik.handleSubmit} className="modify-product__form">
+            <TextField
+              label="Product Title"
+              variant="outlined"
+              name="title"
+              size="medium"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+              style={{ width: "500px", color: "grey", margin: "12px 0px" }}
+              color="primary"
+            />
+            <TextField
+              label="Description"
+              variant="outlined"
+              name="description"
+              multiline
+              rows={5}
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={
+                formik.touched.description && Boolean(formik.errors.description)
+              }
+              helperText={
+                formik.touched.description && formik.errors.description
+              }
+              style={{ width: "500px", color: "grey", margin: "12px 0px" }}
+              color="primary"
+            />
+            <TextField
+              label="Price"
+              variant="outlined"
+              size="medium"
+              type="number"
+              name="price"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && formik.errors.price}
+              InputProps={{
+                inputProps: {
+                  min: 0,
+                },
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              style={{ width: "500px", color: "grey", margin: "12px 0px" }}
+              color="primary"
+            />
+            <TextField
+              label="Image Url"
+              variant="outlined"
+              name="imageUrl"
+              value={formik.values.imageUrl}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.imageUrl && Boolean(formik.errors.imageUrl)}
+              helperText={formik.touched.imageUrl && formik.errors.imageUrl}
+              style={{ width: "500px", color: "grey", margin: "12px 0px" }}
+              color="primary"
+            />
+            <div className="modify-product__button">
+              <input
+                type="button"
+                value="Back"
+                className=" modify-product__button--blue"
+                onClick={handleClose}
+              />
+              <input
+                type="submit"
+                value="Save"
+                className=" modify-product__button--blue"
+              />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      {children}
+    </>,
+    document.getElementById("root")
   );
 };
 
